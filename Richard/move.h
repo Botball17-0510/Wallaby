dfa5d2d8c983e21926b92df9722466322cffeded
Richard Liu <richy.liu.2002@gmail.com>
2017-02-26 21:23:24 -0800

    add code from sunday 2/26 meeting

--- Richard/move.h
+++ Richard/move.h
@@ -1,12 +1,20 @@
-#ifndef COMPASS_H
-#define COMPASS_H
+#ifndef MOVE_H
+#define MOVE_H
 
 
+
+#define RIGHT 0
+#define LEFT 1
+#define TOPHAT 0
 
 #define INCH 166 // # of ticks to move 1 inch
 #define MOVE_DEGREE 7.5 // # of ticks to move the robot 1 degree with both wheels
 
+#define NORMAL_SPEED 1500 // max speed
 
+
+#define TRUE 1
+#define FALSE 0
 
 
 /*
@@ -14,11 +22,13 @@
     
     int inches
         number of inches to move
+    int speed
+        speed to move at
 **/
-void move(int inches) {
-    mrp(RIGHT, NORMAL_SPEED, inches * INCH);
-    mrp(LEFT, NORMAL_SPEED, inches * INCH);
-    msleep(abs(inches) * SLEEP_INCH); // time take to move == # of ticks
+void move(int inches, int speed) {
+    mrp(RIGHT, speed, inches * INCH);
+    mrp(LEFT, speed, inches * INCH);
+    msleep(abs(inches) * INCH); // time take to move == # of ticks
 }
 
 
@@ -68,6 +78,22 @@ void turnOneWheel(float direction, int outside, int speed) {
 
 
 /*
+    Move the servo to a position (warning: delays 0.1 seconds)
+    int servo
+        id of the servo to move
+    int position
+        position to move the servo, between
+**/
+void setServo(int servo, int position) {
+    enable_servo(servo);
+    set_servo_position(servo, position);
+    msleep(100);
+    disable_servo(servo);
+}
+
+
+
+/*
     Calibrate the line follower
     int* white
         the white value for the line follower
@@ -82,9 +108,9 @@ void lineCalibrate(int* white, int* black) {
         printf("%d\n", analog(TOPHAT));
     }
     *white = analog(TOPHAT);
-    printf("White: %d\n", white);
+    printf("White: %d\n", *white);
     msleep(1000);
-    
+
     // calibrate black (> white)
     printf("Calibrate black\n");
     while(!a_button()) {
@@ -92,7 +118,7 @@ void lineCalibrate(int* white, int* black) {
         printf("%d\n", analog(TOPHAT));
     }
     *black = analog(TOPHAT);
-    printf("Black: %d\n", black);
+    printf("Black: %d\n", *black);
     msleep(1000);
 }
 
@@ -101,16 +127,18 @@ void lineCalibrate(int* white, int* black) {
 /*
     Follow the right edge of the line.
     int white
-        the white value for the line follower
+        White value for the line follower
     int black
-        the black value for the line follower
+        Black value for the line follower
+    int speed
+        base speed the line follower is going at
+    int sensitivity
+        how much the speed can vary
+    int totalTime
+        how long the line follower can run
 **/
-void lineFollow(int white, int black) {
-    // recommended sensitivity >= speed
-    const int speed = 700; // base speed; following line going at this speed
-    const int sensitivity = 800; // how much the speed can change by
-    // this means that speed can at most be (speed + sensitivity) and at least
-    // be (speed - sensitivity)
+void lineFollow(int white, int black, int speed, int sensitivity, int totalTime) {
+    int timeElapsed = 0;
     
     printf("Starting line follower\n");
     while(TRUE) {
@@ -118,14 +146,18 @@ void lineFollow(int white, int black) {
         // ensure light is within range from 0 to 1
         if (light < 0) light = 0;
         if (light > 1) light = 1;
-        
+
         light -= 0.5; // change to number between -0.5 and 0.5
-        
+
         // follows on left border
         mav(RIGHT, speed + (light * sensitivity));
         mav(LEFT, speed - (light * sensitivity));
-        
+
         msleep(5);
+        timeElapsed += 5;
+        if (timeElapsed > totalTime) {
+            break;
+        }
     }
 }
 
---
+++ Richard/moveUpRamp.c
@@ -0,0 +1,26 @@
+#include <kipr/botball.h>
+#include "move.h"
+
+#define ARM 0
+
+int main() {
+  set_servo_position(ARM, 1100);
+  enable_servo(ARM);
+  
+  move(30, 700);
+  
+  set_servo_position(ARM, 1800);
+  
+  
+  int white = 2220;
+  int black = 3630;
+  //lineCalibrate(&white, &black);
+  
+  lineFollow(white, black, 800, 300, 4500);
+  
+  move(10, 1500);
+  
+  lineFollow(white, black, 600, 400, 4000);
+  
+  return 0;
+}
