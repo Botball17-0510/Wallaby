7dad134f515ae181b68b7e1e1e0ffdd2ae22d5fc
Richard Liu <richy.liu.2002@gmail.com>
2017-03-24 21:30:04 -0700

    tweak params

--- bin/minified.c
+++ bin/minified.c
@@ -89,26 +89,20 @@ void lineFollow(int white, int black, int speed, int sensitivity, int totalTime)
 #define ARM_45 1300
 #define ARM_30 1000
 #define ARM_0 900
-#define ARM_N10 780 // negative 10
+#define ARM_N10 750 // negative 10
 #define ARM_N30 600 // negative 30
 
 #define CLAW_OPEN 1200
 #define CLAW_CLOSED 550
 
+/* wire position
+ * - one that goes around the light sensor: left port and black facing
+ * - the other one: right port and red facing
+ */
+
 
 
 int main() {
-    printf("*******\n");
-    printf("*******\n");
-    printf("*******\n");
-    printf("*******\n");
-    printf("*******\n");
-    printf("*******\n");
-    printf("*******\n");
-    printf("*******\n");
-    printf("*******\n");
-    return 0;
-    
     // calibrate line follower
     int white = 2220;
     int black = 3630;
@@ -128,22 +122,21 @@ int main() {
     // grab pom
     slowServo(CLAW, CLAW_OPEN, CLAW_CLOSED, 10);
     msleep(400);
-    slowServo(ARM, ARM_N10, ARM_90, 200);
+    slowServo(ARM, ARM_N10, ARM_90, 100);
 
     // move forward: slowly at first, then faster
-    move(5, 800);
-    move(10, 1500);
+    // move(5, 800);
+    move(17, 1500);
     // set arm down
-    set_servo_position(ARM, ARM_30);
-    msleep(300);
-    set_servo_position(ARM, ARM_0);
+    slowServo(ARM, ARM_90, ARM_0, 500);
 
 
     // line follow until top of ramp
     lineFollow(white, black, 400, 1400, 8500);
+    move(7, 1500);
     // open claw
     set_servo_position(CLAW, CLAW_OPEN);
-    move(10, 1500);
+    move(7, 1500);
     // set arm down
     set_servo_position(ARM, ARM_N30);
     msleep(500);
@@ -168,29 +161,33 @@ int main() {
     
 
     // line follow until planter bin (it's okay if it runs over a few poms)
-    lineFollow(white, black, 400, 600, 2000);
+    lineFollow(white, black, 400, 600, 2200);
     msleep(500);
     // move back a bit
-    move(-3, 700);
+    move(-4, 700);
     
     // close claw
     slowServo(CLAW, CLAW_OPEN, CLAW_CLOSED, 400);
     msleep(800);
     // raise arm
-    slowServo(ARM, ARM_N10, ARM_45, 1000);
+    slowServo(ARM, ARM_N10, ARM_90, 1000);
     msleep(1000);
     
     // move forward
-    move(5, 800);
+    move(6, 800);
     msleep(500);
     // drop poms
+    set_servo_position(ARM, ARM_45);
+    msleep(500);
     set_servo_position(CLAW, CLAW_OPEN);
     msleep(500);
     set_servo_position(ARM, ARM_135);
     msleep(500);
     
     // move back
-    move(-20, 700);
+    move(-15, 700);
+    // turn left a bit to get light sensor on line
+    turnOneWheel(-5, 1, 700);
     
     
     // REPEAT
